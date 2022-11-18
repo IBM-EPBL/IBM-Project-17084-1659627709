919746a752fa4b4153d91ac2e7946d05315de70c
Devi ansuha <anushadevi02002@gmail.com>
2022-11-18 14:43:17 +0530

    commit

--- Project_development_phase/Airineswebpage/script.js
+++ Project_development_phase/Airineswebpage/script.js
@@ -1,6 +1,9 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+const loginButton1 = document.getElementById("login-form-submit1");
+
+
+valid={'ad':'ad123' , 'admin':'admin123', 'ad1':'ad1-123' ,'user':'pswd'}
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -32,5 +35,32 @@ loginButton.addEventListener("click", (e) => {
         document.getElementById("login-error-msg").innerHTML=u+p+'Invalid username <span id="error-msg-second-line">and/or password</span>';
         document.getElementsByName("username").value="";
         document.getElementsByName("username").value="";
+    }
+})
+
+loginButton1.addEventListener("click", (e) => {
+    e.preventDefault();
+    var un = document.getElementById("uname").value;
+    var ps = document.getElementById("pass").value;
+    var rps = document.getElementById("rpass").value;
+    var i=1;
+    for (let x in valid) {
+        if(x==un) {
+            i=0;
+            document.getElementById("login-error-msg").innerHTML='User already exists';
+            document.getElementsByName("username").value="";
+            document.getElementsByName("username").value="";
+        }
+    }
+    if(i==1) {
+        if(ps!=rps) {
+            document.getElementById("login-error-msg").innerHTML='Password Mismatch';
+            document.getElementsByName("username").value="";
+            document.getElementsByName("username").value="";
+        }
+        else {
+            valid[un]=ps;
+            document.getElementById("login-error-msg").innerHTML='Sign-up Successful';
+        }
     }
 })
